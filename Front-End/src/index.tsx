8cf0bd4cf57cade08fac29a984d21bab99a47b8e
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-08-19 23:59:18 -0600

    Import app styles

--- Front-End/src/index.tsx
+++ Front-End/src/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+
+import App from "./App";
+import "./sass/styles.scss"
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
