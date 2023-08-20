bfc3f7408051aa1cad7b04786dcd74460fabb6f4
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-08-20 14:14:57 -0600

    Add home page

---
+++ Front-End/src/components/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MainPage from "../templates/MainPage/MainPage";
+
+/** Component that render the home page */
+const Home = () => {
+  return (
+    <MainPage>
+      <h1>LabTrack</h1>
+      <p>Esta es la página de inicio.</p>
+    </MainPage>
+  );
+};
+
+export default Home;
