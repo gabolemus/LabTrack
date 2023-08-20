37a82bf33365a1781de2a0d18591bec3c2abaf77
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-08-20 14:07:52 -0600

    Add doc comment

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import { pages } from "./utils/pages";
 
+/** Component that render the web application */
 const App = () => {
   return (
     <Routes>
