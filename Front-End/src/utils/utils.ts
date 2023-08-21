126c1358c52de2d30811cda3496ebca68d8f99b0
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-08-20 21:28:52 -0600

    Add function to convert timestamp to a date

--- Front-End/src/utils/utils.ts
+++ Front-End/src/utils/utils.ts
@@ -13,3 +13,24 @@ export const roundToDecimalPlaces = (
   const factor = 10 ** decimalPlaces;
   return Math.round(num * factor) / factor;
 };
+
+/**
+ * Converts a timestamp to a date string.
+ * The format used is YYYY-MM-DDTHH:mm:ss.sssZ.
+ * @param timestamp Timestamp to convert.
+ * @returns Date string.
+ */
+export const timestampToDate = (timestamp: string): string => {
+  // E.g. 2021-08-01T12:00:00.000Z --> 1 de agosto de 2021 a las 12:00:00
+  const date = new Date(timestamp);
+  const day = date.getDate();
+  const month = date.toLocaleString("es-ES", { month: "long" });
+  const year = date.getFullYear();
+  const time = date.toLocaleTimeString("es-ES", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+
+  return `${day} de ${month} de ${year} a las ${time}`;
+};
