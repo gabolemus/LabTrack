e89f846fb6bfc2ece6592753d3a8eda3c76d1b39
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-08-20 21:29:06 -0600

    Update projects component

--- Front-End/src/components/pages/Projects.tsx
+++ Front-End/src/components/pages/Projects.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import MainPage from "../templates/MainPage/MainPage";
+import ProjectsList from "../organisms/ProjectsList/ProjectsList";
+import ProjectDetail from "../organisms/ProjectDetail/ProjectDetail";
 
 const Projects = () => {
   const { projectId } = useParams();
 
   return (
     <MainPage>
-      <h1>Proyectos</h1>
-      <p>Proyecto: {projectId}</p>
-      <p>Este es el listado de proyectos.</p>
+      {projectId ? <ProjectDetail id={projectId} /> : <ProjectsList />}
     </MainPage>
   );
 };
