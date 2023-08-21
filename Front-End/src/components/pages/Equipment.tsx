2760b9f27be5f3a70ac813f256034271b05b3645
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-08-20 21:29:16 -0600

    Update equipment component

--- Front-End/src/components/pages/Equipment.tsx
+++ Front-End/src/components/pages/Equipment.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import MainPage from "../templates/MainPage/MainPage";
+import EquipmentList from "../organisms/EquipmentList/EquipmentList";
+import EquipmentDetail from "../organisms/EquipmentDetail/EquipmentDetail";
 
 const Equipment = () => {
   const { equipmentId } = useParams();
 
   return (
     <MainPage>
-      <h1>Equipo de Laboratorio</h1>
-      <p>Equipo: {equipmentId}</p>
-      <p>Este es el listado de equipos de laboratorio.</p>
+      {equipmentId ? <EquipmentDetail id={equipmentId} /> : <EquipmentList />}
     </MainPage>
   );
 };
