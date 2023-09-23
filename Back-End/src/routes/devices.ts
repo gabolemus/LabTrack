adf40410855e643213b266f0d1402ab84db0c77c
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-09-22 18:07:19 -0600

    Add logger messages

--- Back-End/src/routes/devices.ts
+++ Back-End/src/routes/devices.ts
@@ -1,12 +1,16 @@
 import express from "express";
 
+import logger from "../utils/logger";
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  logger.info("GET /devices");
   res.send("Devices home page");
 });
 
 router.get("/:id", (req, res) => {
+  logger.info(`GET /devices/${req.params.id}`);
   res.send(`Device with id ${req.params.id}`);
 });
 
