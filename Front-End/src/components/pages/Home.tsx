5abe140e7df7a095aa9f5499dafd16c63bfccadb
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-09-24 16:41:28 -0600

    Update home page content

--- Front-End/src/components/pages/Home.tsx
+++ Front-End/src/components/pages/Home.tsx
@@ -6,7 +6,29 @@ const Home = () => {
   return (
     <MainPage>
       <h1>LabTrack</h1>
-      <p>Esta es la página de inicio.</p>
+      <p>
+        LabTrack es el sistema de registro y control del equipo del laboratorio
+        de electrónica.
+      </p>
+      <h2>Funcionalidades</h2>
+      <ul>
+        <li>
+          <strong>Visualizacíon del equipo:</strong> permite ver el equipo del
+          laboratorio.
+        </li>
+        <li>
+          <strong>Gestión de proyectos:</strong> es posible visualizar los
+          detalles de los proyectos realizados en el laboratorio.
+        </li>
+        <li>
+          <strong>Gestión de equipos:</strong> permite visualizar los detalles
+          de los equipos del laboratorio.
+        </li>
+        <li>
+          <strong>Solicitud de equipos:</strong> permite solicitar equipos del
+          laboratorio para la realización de proyectos.
+        </li>
+      </ul>
     </MainPage>
   );
 };
