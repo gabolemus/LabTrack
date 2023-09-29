c4c6bc7e8f53842d75ae409ab28d189926196389
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-09-28 22:30:13 -0600

    Add API requests

--- Front-End/src/components/organisms/ProjectsList/projects.ts
+++ Front-End/src/components/organisms/ProjectsList/projects.ts
@@ -1,87 +1,71 @@
 // This file contains the type definitions for the Project component.
 
+import axios from "axios";
+
 /** Type definition for the equipment used in a project */
-type EquipmentUsed = {
-  id: string;
+type ProjectDevices = {
+  _id: string;
   name: string;
-  quantity: number;
   path: string;
+  quantity: number;
+};
+
+/** Type that represents the project's lead information */
+type Lead = {
+  name: string;
+  email: string;
+};
+
+/** Type that represents the project's timelapse */
+type Timelapse = {
+  start: string;
+  end: string;
 };
 
 /** Project type definition */
 export type Project = {
-  id: string;
+  _id: string;
   name: string;
+  path: string;
   description: string;
-  lead: string;
-  startDate: string;
-  dueDate: string;
+  lead: Lead;
+  timelapse: Timelapse;
+  status: string;
   active: boolean;
   notes: string;
-  equipmentUsed: EquipmentUsed[];
+  devices: ProjectDevices[];
 };
 
 /** Sample project data */
-export const sampleProjectData = [
-  {
-    id: "proyecto-1",
-    name: "Proyecto 1",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros quis nisl aliquam aliquet. Sed euismod, nisl vitae aliquam aliquet, nisl nisl aliquam nisl, vitae aliquam nisl nisl vitae nisl. Sed vitae eros quis nisl aliquam aliquet. Sed euismod, nisl vitae aliquam aliquet, nisl nisl aliquam nisl, vitae aliquam nisl nisl vitae nisl.",
-    lead: "John Doe",
-    startDate: "2023-01-02",
-    dueDate: "2023-06-15",
-    active: false,
-    notes:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros quis nisl aliquam aliquet. Sed euismod, nisl vitae aliquam aliquet, nisl nisl aliquam nisl, vitae aliquam nisl nisl vitae nisl. Sed vitae eros quis nisl aliquam aliquet. Sed euismod, nisl vitae aliquam aliquet, nisl nisl aliquam nisl, vitae aliquam nisl nisl vitae nisl.",
-    equipmentUsed: [
-      {
-        id: "cisco-catalyst-9400-series-switch",
-        name: "Cisco Catalyst 9400 Series Switch",
-        quantity: 2,
-        path: "/equipment/cisco-catalyst-9400-series-switch",
-      },
-      {
-        id: "cisco-catalyst-9300-series-switch",
-        name: "Cisco Catalyst 9300 Series Switch",
-        quantity: 1,
-        path: "/equipment/cisco-catalyst-9300-series-switch",
-      },
-    ],
-  },
-  {
-    id: "proyecto-2",
-    name: "Proyecto 2",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros quis nisl aliquam aliquet. Sed euismod, nisl vitae aliquam aliquet, nisl nisl aliquam nisl, vitae aliquam nisl nisl vitae nisl. Sed vitae eros quis nisl aliquam aliquet. Sed euismod, nisl vitae aliquam aliquet, nisl nisl aliquam nisl, vitae aliquam nisl nisl vitae nisl.",
-    lead: "Jane Smith",
-    startDate: "2023-01-01",
-    dueDate: "2023-12-31",
-    active: true,
-    notes:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros quis nisl aliquam aliquet. Sed euismod, nisl vitae aliquam aliquet, nisl nisl aliquam nisl, vitae aliquam nisl nisl vitae nisl. Sed vitae eros quis nisl aliquam aliquet. Sed euismod, nisl vitae aliquam aliquet, nisl nisl aliquam nisl, vitae aliquam nisl nisl vitae nisl.",
-    equipmentUsed: [
-      {
-        id: "oceanstor-dorado-3000-v6",
-        name: "OceanStor Dorado 3000 V6",
-        quantity: 1,
-        path: "/equipment/oceanstor-dorado-3000-v6",
-      },
-    ],
-  },
-];
+export const sampleProjectData: Project[] = [];
+
+/** Gets all the projects */
+export const getProjects = async (): Promise<Array<Project>> => {
+  try {
+    const response = await axios.get("http://localhost:8080/projects");
+    response.data.projects.forEach((project: Project) => {
+      if (project.status === "In Progress") {
+        project.active = true;
+      } else {
+        project.active = false;
+      }
+    });
+    return response.data.projects as Array<Project>;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
 
 /** Fetches equipment data from the server */
 export const fetchProject = async (id: string): Promise<Project> => {
-  // TODO: Implement fetch from server
-
-  // Right now, we just return the item that matches the id
-  return new Promise((resolve, reject) => {
-    const item = sampleProjectData.find((item) => item.id === id);
-    if (item) {
-      resolve(item);
-    } else {
-      reject(new Error("Item not found"));
-    }
-  });
+  try {
+    const response = await axios.get(`http://localhost:8080/project/${id}`);
+    response.data.project.active =
+      response.data.project.status === "In Progress";
+    return response.data.project as Project;
+  } catch (error) {
+    console.log(error);
+    return {} as Project;
+  }
 };
