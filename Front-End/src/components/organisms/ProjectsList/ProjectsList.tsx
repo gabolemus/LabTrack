5faa9713ea8badcb942d85ca1d2249b3ab29522d
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-09-29 00:24:26 -0600

    Remove sample data

--- Front-End/src/components/organisms/ProjectsList/ProjectsList.tsx
+++ Front-End/src/components/organisms/ProjectsList/ProjectsList.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Project, getProjects, sampleProjectData } from "./projects";
+import { Project, getProjects } from "./projects";
 
 import "./ProjectsList.scss";
 
