8bbb549f992e2a2da26f3cced41aa14dc221a6c8
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-10-06 22:02:15 -0600

    Add admins page

---
+++ Front-End/src/components/pages/Admins.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MainPage from "../templates/MainPage/MainPage";
+import AdminsLists from "../organisms/AdminsLists/AdminsLists";
+
+const Admins = () => {
+  return (
+    <MainPage>
+      <AdminsLists />
+    </MainPage>
+  );
+};
+
+export default Admins;
