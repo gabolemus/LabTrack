e6d0a81bce1a03e68c856ca499e16bbc106d5f5b
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-10-07 16:26:28 -0600

    Add a function to check if 2 arrays of objects are equal

--- Front-End/src/utils/utils.ts
+++ Front-End/src/utils/utils.ts
@@ -54,3 +54,54 @@ export const toggleLoader = (
     document.body.classList.remove("no-scroll");
   }
 };
+
+/**
+ * Checks if 2 objects are equal. AKA checks if all the key-value pairs of the
+ * objects are equal.
+ * @param objA First object.
+ * @param objB Second object.
+ * @returns true if the objects are equal, false otherwise.
+ */
+const areObjectsEqual = <T extends Record<string, unknown>>(
+  objA: T,
+  objB: T
+): boolean => {
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
+
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  for (const key of keysA) {
+    if (objA[key] !== objB[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+/**
+ * Checks if 2 arrays of objects are equal. AKA checks if all the key-value pairs
+ * of the objects in the arrays are equal.
+ * @param arrA First array.
+ * @param arrB Second array.
+ * @returns true if the arrays are equal, false otherwise.
+ */
+export const areArraysEqual = <T extends Record<string, unknown>>(
+  arrA: Array<T>,
+  arrB: Array<T>
+): boolean => {
+  if (arrA.length !== arrB.length) {
+    return false;
+  }
+
+  for (const [index, value] of arrA.entries()) {
+    if (!areObjectsEqual(value, arrB[index])) {
+      return false;
+    }
+  }
+
+  return true;
+};
