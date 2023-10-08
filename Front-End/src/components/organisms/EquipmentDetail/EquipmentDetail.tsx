077fa7ef9c80ba3286d557f27d27a7b9c5c2b7d0
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-10-08 12:21:25 -0600

    Update component to allow to udpate the equipment data

--- Front-End/src/components/organisms/EquipmentDetail/EquipmentDetail.tsx
+++ Front-End/src/components/organisms/EquipmentDetail/EquipmentDetail.tsx
@@ -1,30 +1,57 @@
 import React, { useEffect, useState } from "react";
 import { Equipment, fetchEquipmentData } from "../EquipmentList/equipment";
-
-import "./EquipmentDetail.scss";
 import EquipmentImgView from "../EquipmentImgView/EquipmentImgView";
 import EquipmentDocs from "../../molecules/EquipmentDocs/EquipmentDocs";
-import { timestampToDate } from "../../../utils/utils";
+import { areObjectsEqual, timestampToDate } from "../../../utils/utils";
 import { Link } from "react-router-dom";
+import "./EquipmentDetail.scss";
+import Loader from "../../molecules/Loader/Loader";
+import ModalForm from "../ModalForm/ModalForm";
+import {
+  Manufacturer,
+  getAllManufacturers,
+} from "../ManufacturersList/manufacturers";
+import axios from "axios";
 
 /** Interface for EquipmentDetail props */
 interface EquipmentDetailProps {
+  /** Device ID */
   id: string;
+  /** Whether to allow the user to update the device */
+  allowUpdate?: boolean;
 }
 
-const EquipmentDetail = ({ id }: EquipmentDetailProps) => {
-  const [equipment, setEquipment] = useState<Equipment | null>(null);
+const EquipmentDetail = ({ id, allowUpdate }: EquipmentDetailProps) => {
+  const [device, setDevice] = useState<Equipment>({} as Equipment);
+  const [updatedDevice, setUpdatedDevice] = useState<Equipment>(
+    {} as Equipment
+  );
+  const [manufacturers, setManufacturers] = useState<Array<Manufacturer>>([]);
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  const [modalCallback, setModalCallback] = useState<() => void>(() => {});
+  const [modalTitle, setModalTitle] = useState("");
+  const [modalBody, setModalBody] = useState(<></>);
+  const [modalBtnText, setModalBtnText] = useState("");
+  const [modalBtnClass, setModalBtnClass] = useState("");
 
   useEffect(() => {
-    // Scroll to top
-    window.scrollTo(0, 0);
+    // // Scroll to top
+    // window.scrollTo(0, 0);
 
     // Simulate fetching equipment data based on the ID from your data source
     const fetchData = async () => {
       try {
+        // Get the equipment
         const data = await fetchEquipmentData(id);
-        setEquipment(data);
+        setDevice(data);
+        setUpdatedDevice(data);
+
+        // Get the manufacturers
+        const manufacturersData = await getAllManufacturers();
+        setManufacturers(manufacturersData);
 
         // Select the first image as the initially selected image
         if (data.images && data.images.length > 0) {
@@ -35,10 +62,12 @@ const EquipmentDetail = ({ id }: EquipmentDetailProps) => {
       }
     };
 
+    setLoading(true);
     fetchData();
+    setLoading(false);
   }, [id]);
 
-  if (!equipment) {
+  if (!device) {
     // Equipment not found by ID
     return (
       <>
@@ -74,58 +103,232 @@ const EquipmentDetail = ({ id }: EquipmentDetailProps) => {
     }
   };
 
-  return (
-    <div className="equipment-detail mb-5">
-      <h1>{equipment.name}</h1>
-      <EquipmentImgView
-        equipment={equipment}
-        selectedImage={selectedImage}
-        handleImageClick={handleImageClick}
-      />
-      {equipment.images && <hr className="divider" />}
-      <div className="equipment-details">
-        <h2>Detalles del Equipo</h2>
-        <p>
-          <b>Fabricante:</b> {equipment.manufacturer}
-        </p>
-        <EquipmentDocs equipment={equipment} />
-        <hr className="divider" />
-        <h2>Proyectos en los que se ha utilizado</h2>
-        {equipment.projects && equipment.projects.length > 0 ? (
-          <ul className="mb-4">
-            {equipment.projects.map((project) => (
-              <li key={project.name}>
-                <Link to={`/projects${project.path}`}>{project.name}</Link>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No se ha utilizado en ningún proyecto</p>
-        )}
-        <hr className="divider" />
-        <h2>Información Adicional</h2>
-        {equipment.notes && (
-          <div className="notes">
-            <h3>Notas</h3>
-            <p className="equipment-notes">{equipment.notes}</p>
-          </div>
-        )}
-        <div className="status mb-5">
-          <h3>Estado</h3>
-          <p>{getDeviceStatus(equipment.status)}</p>
-        </div>
-        <div className="last-checked mb-5">
-          <h3>Última Actualización</h3>
-          <p>{timestampToDate(equipment.updatedAt)}</p>
-        </div>
-        {equipment.configuration && (
-          <div className="configuration">
-            <h3>Configuration</h3>
-            <p className="equipment-notes">{equipment.configuration}</p>
-          </div>
-        )}
-      </div>
+  /** Undoes any changes made to the device */
+  const discardChanges = () => {
+    setUpdatedDevice(device);
+  };
+
+  /** Component for the update buttons */
+  const UpdateButtons = () => (
+    <div className="d-flex justify-content-between align-items-center mb-3">
+      <button
+        className="btn btn-primary"
+        disabled={areObjectsEqual(device, updatedDevice)}
+        onClick={handleUpdateDevice}>
+        Actualizar
+      </button>
+      <button
+        className="btn btn-danger"
+        disabled={areObjectsEqual(device, updatedDevice)}
+        onClick={discardChanges}>
+        Descartar Cambios
+      </button>
     </div>
+  );
+
+  /** Modal callback to update the device */
+  const updateDevice = async (device: Equipment) => {
+    // Decode the images URLs before sending the request as they are encoded in the BE
+    if (device.images) {
+      device.images = device.images.map((image) => decodeURIComponent(image));
+    }
+
+    try {
+      const response = await axios.put(
+        `http://localhost:8080/device?id=${device._id}`,
+        device
+      );
+      const data = response.data;
+      setShowModal(true);
+
+      if (response.status === 200 && data.success) {
+        setDevice(data.device);
+        setUpdatedDevice(data.device);
+        setModalTitle("Equipo Actualizado");
+        setModalBody(
+          <p className="fs-6">El equipo fue actualizado correctamente.</p>
+        );
+        setModalBtnText("Aceptar");
+        setModalBtnClass("btn-success");
+        setModalCallback(() => () => {
+          setShowModal(false);
+        });
+      }
+    } catch (error) {
+      console.error(error);
+
+      let msg = (
+        <>
+          <p className="fs-6">
+            Ha ocurrido un error al intentar agregar el equipo.
+          </p>
+          <p className="fs-6">Por favor, intente de nuevo más tarde.</p>
+        </>
+      );
+
+      if (axios.isAxiosError(error)) {
+        const { response } = error;
+
+        if (response?.data.error === "ENFORCE_UNIQUE_FIELD") {
+          msg = (
+            <>
+              <p className="fs-6">
+                Un dispositivo con el nombre {device.name} ya existe.
+              </p>
+              <p className="fs-6">
+                Por favor, ingrese un nombre diferente para el dispositivo.
+              </p>
+            </>
+          );
+        }
+      }
+
+      setModalTitle("Error");
+      setModalBody(msg);
+      setModalBtnText("Aceptar");
+      setModalBtnClass("btn-danger");
+      setModalCallback(() => () => {
+        setShowModal(false);
+      });
+    }
+  };
+
+  /** Handles updating the device */
+  const handleUpdateDevice = () => {
+    setShowModal(true);
+    setModalTitle("Actualizar Equipo");
+    setModalBody(
+      <p className="fs-6 ">
+        ¿Estás seguro de que quieres actualizar este equipo?
+      </p>
+    );
+    setModalBtnText("Actualizar");
+    setModalBtnClass("btn-success");
+    setModalCallback(() => () => updateDevice(updatedDevice));
+  };
+
+  return !loading ? (
+    <>
+      <ModalForm
+        show={showModal}
+        handleClose={() => setShowModal(false)}
+        onAccept={modalCallback}
+        title={modalTitle}
+        body={modalBody}
+        primaryBtnText={modalBtnText}
+        primaryBtnClass={modalBtnClass}
+      />
+      <Loader loading={loading} />
+      <div className="equipment-detail mb-5">
+        {allowUpdate ? (
+          <>
+            <UpdateButtons />
+            <div className="d-flex justify-content-between align-items-center">
+              <input
+                className="form-control mb-5 device-title-input"
+                type="text"
+                value={updatedDevice.name}
+                onChange={(e) =>
+                  setUpdatedDevice({ ...updatedDevice, name: e.target.value })
+                }
+              />
+            </div>
+          </>
+        ) : (
+          <h1>{device.name}</h1>
+        )}
+        <EquipmentImgView
+          equipment={device}
+          selectedImage={selectedImage}
+          handleImageClick={handleImageClick}
+        />
+        {device.images && <hr className="divider" />}
+        <div className="equipment-details">
+          <h2>Detalles del Equipo</h2>
+          {allowUpdate ? (
+            <div className="mb-5">
+              <label htmlFor="manufacturer">Fabricante</label>
+              <select
+                className="form-control mb-5"
+                value={updatedDevice.manufacturer}
+                onChange={(e) => {
+                  setUpdatedDevice({
+                    ...updatedDevice,
+                    manufacturer: e.target.value,
+                  });
+                }}>
+                {manufacturers.map((manufacturer) => (
+                  <option key={manufacturer._id} value={manufacturer.name}>
+                    {manufacturer.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          ) : (
+            <p>
+              <b>Fabricante:</b> {device.manufacturer}
+            </p>
+          )}
+          <EquipmentDocs equipment={device} />
+          <hr className="divider" />
+          <h2>Proyectos en los que se ha utilizado</h2>
+          {device.projects && device.projects.length > 0 ? (
+            <ul className="mb-4">
+              {device.projects.map((project) => (
+                <li key={project.name}>
+                  <Link to={`/projects${project.path}`}>{project.name}</Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No se ha utilizado en ningún proyecto</p>
+          )}
+          <hr className="divider" />
+          <h2>Información Adicional</h2>
+          {device.notes && (
+            <div className="notes">
+              <h3>Notas</h3>
+              <p className="equipment-notes">{device.notes}</p>
+            </div>
+          )}
+          <div className="status mb-5">
+            <h3>Estado</h3>
+            {allowUpdate ? (
+              <select
+                className="form-select"
+                id="deviceStatus"
+                value={updatedDevice.status}
+                onChange={(e) => {
+                  setUpdatedDevice({
+                    ...updatedDevice,
+                    status: e.target.value,
+                  });
+                }}>
+                <option value="Available">Disponible</option>
+                <option value="In Use">En uso</option>
+                <option value="In Maintenance">En mantenimiento</option>
+                <option value="Broken">Averiado</option>
+              </select>
+            ) : (
+              <p>{getDeviceStatus(device.status)}</p>
+            )}
+          </div>
+          <div className="last-checked mb-5">
+            <h3>Última Actualización</h3>
+            <p>{timestampToDate(device.updatedAt)}</p>
+          </div>
+          {device.configuration && (
+            <div className="configuration">
+              <h3>Configuration</h3>
+              <p className="equipment-notes">{device.configuration}</p>
+            </div>
+          )}
+          {allowUpdate && <UpdateButtons />}
+        </div>
+      </div>
+    </>
+  ) : (
+    <Loader loading={loading} />
   );
 };
 
