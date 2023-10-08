ca7d05299ddc8b07ed9a6fcb80158a880ccb0c91
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-10-08 15:15:01 -0600

    Add login/logout functionality

--- Front-End/src/components/organisms/Navbar/Navbar.tsx
+++ Front-End/src/components/organisms/Navbar/Navbar.tsx
@@ -1,10 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.scss";
-
 import { pages } from "../../../utils/pages";
+import { useState } from "react";
+
+/** Interface for the props of the Navbar component */
+interface Props {
+  /** Whether to show the login button */
+  showLogin: boolean;
+}
 
 /** Component that renders the navbar */
-const Navbar = () => {
+const Navbar = ({ showLogin }: Props) => {
+  const [showLoginBtn, setShowLoginBtn] = useState(showLogin);
+  const navigate = useNavigate();
+
+  /** Callback to logout the user */
+  const logout = () => {
+    localStorage.removeItem("logged");
+    localStorage.removeItem("email");
+    localStorage.removeItem("role");
+    setShowLoginBtn(true);
+    navigate("/");
+  };
+
   return (
     <nav id="navbar" className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
@@ -35,6 +53,15 @@ const Navbar = () => {
               </li>
             ))}
           </ul>
+          {(showLoginBtn && (
+            <Link className="btn btn-outline-primary me-2" to="/login">
+              Iniciar sesión
+            </Link>
+          )) || (
+            <button className="btn btn-outline-primary me-2" onClick={logout}>
+              Cerrar sesión
+            </button>
+          )}
         </div>
       </div>
     </nav>
--- Front-End/src/components/templates/MainPage/MainPage.tsx
+++ Front-End/src/components/templates/MainPage/MainPage.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import Navbar from "../../organisms/Navbar/Navbar";
 
 import "./MainPage.scss";
@@ -6,20 +6,24 @@ import "./MainPage.scss";
 interface Props {
   /** Element(s) to be rendered inside the main page */
   children: ReactNode | ReactNode[];
-  /** Whether to show the login button */
-  showLogin?: boolean;
-  /** Whether to show the logout button */
-  showLogout?: boolean;
 }
 
 /**
  * Template for the components that will be rendered inside the main page
  * @param {Props} props - Properties passed to component
  */
-const MainPage: React.FC<Props> = ({ children, showLogin, showLogout }: Props) => {
+const MainPage: React.FC<Props> = ({ children }: Props) => {
+  const [showLogin, setShowLogin] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("logged") === null) {
+      setShowLogin(true);
+    }
+  }, []);
+
   return (
     <>
-      <Navbar showLogin={showLogin} showLogout={showLogout} />
+      <Navbar showLogin={showLogin} />
       <main className="container">{children}</main>
     </>
   );
