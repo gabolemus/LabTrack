3037be629be2a3afd4ae44bf2ffed050254194cb
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-10-13 22:56:51 -0600

    Update component to allow filtering requests

--- Front-End/src/components/organisms/InquiriesList/InquiriesList.tsx
+++ Front-End/src/components/organisms/InquiriesList/InquiriesList.tsx
@@ -1,139 +1,99 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { IInquiry, getInquiries } from "./inquiries";
-
+import { IInquiry, InquiryStatus, getFilteredInquiries } from "./inquiries";
 import "./InquiriesList.scss";
+import Loader from "../../molecules/Loader/Loader";
 
 const InquiriesList = () => {
   const [inquiries, setInquiries] = useState<Array<IInquiry>>([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [searchLead, setSearchLead] = useState<string[]>([]);
-  const [searchActive, setSearchActive] = useState<string[]>([]);
-  const [filteredInquiries, setFilteredInquiries] = useState<Array<IInquiry>>([]);
+  const [lastSearchTerm, setLastSearchTerm] = useState("");
+  const [currSearchTerm, setCurrSearchTerm] = useState("");
+  const [lastState, setLastState] = useState("Pending");
+  const [currState, setCurrState] = useState("Pending");
+  const [showPending, setShowPending] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   // Function to handle form submission
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+    filterInquiries();
   };
 
   useEffect(() => {
-    (async () => {
-      const inquiries: Array<IInquiry> = await getInquiries();
-      setInquiries(inquiries);
-      setFilteredInquiries(inquiries);
-    })();
+    filterInquiries();
   }, []);
 
-  // // Get unique project leads and active for checkboxes
-  // const leads = [...new Set(inquiries.map((project) => project.lead))];
-  // const active = [...new Set(inquiries.flatMap((project) => project.active))];
+  /** Filters inquiries by project name and status */
+  const filterInquiries = async () => {
+    setLoading(true);
+    const inquiries: Array<IInquiry> = await getFilteredInquiries(
+      currSearchTerm,
+      showPending ? InquiryStatus.PENDING : InquiryStatus.ACCEPTED
+    );
+    setLastState(currState);
+    setInquiries(inquiries);
+    setLastSearchTerm(currSearchTerm);
+    setLoading(false);
+  };
+
+  /** Determines if a search can be made */
+  const canMakeSearch = () =>
+    (currSearchTerm !== "" && currSearchTerm !== lastSearchTerm) ||
+    currState !== lastState;
 
   return (
     <div>
+      <Loader loading={loading} />
       <h1>Solicitudes de Proyecto</h1>
-      <form className="project-search-form" onSubmit={handleSubmit}>
-        <div className="row mb-3">
-          <div className="col-md-4">
-            <input
-              type="text"
-              id="searchTerm"
-              className="form-control project-search-btn"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-              placeholder="Nombre del proyecto"
-            />
-          </div>
-          <div className="col-md-4">
-            <div className="dropdown">
-              <button
-                className="btn btn-secondary dropdown-toggle project-lead-dropdown"
-                type="button"
-                id="manufacturerDropdown"
-                data-bs-toggle="dropdown"
-                aria-expanded="false">
-                Responsable
-              </button>
-              <ul
-                className="dropdown-menu"
-                aria-labelledby="manufacturerDropdown">
-                {/* {leads.map((lead) => (
-                  <li key={lead.name}>
-                    <a className="dropdown-item" href="#">
-                      <div className="form-check">
-                        <input
-                          className="form-check-input"
-                          type="checkbox"
-                          value={lead.name}
-                          checked={searchLead.includes(lead.name)}
-                          onChange={() =>
-                            setSearchLead((prevSelected) => {
-                              if (prevSelected.includes(lead.name)) {
-                                return prevSelected.filter(
-                                  (l) => l !== lead.name
-                                );
-                              } else {
-                                return [...prevSelected, lead.name];
-                              }
-                            })
-                          }
-                        />
-                        <label className="form-check-label">{lead.name}</label>
-                      </div>
-                    </a>
-                  </li>
-                ))} */}
-              </ul>
+      <form className="inquiry-search-form" onSubmit={handleSubmit}>
+        <div className="row mb-3" id="inquiry-search-form">
+          <div className="col">
+            <div className="input-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Nombre del proyecto"
+                aria-label="Nombre del proyecto"
+                aria-describedby="button-addon2"
+                onChange={(e) => setCurrSearchTerm(e.target.value)}
+              />
+              <div className="input-group-append">
+                <div className="btn-group dropdown">
+                  <button
+                    type="button"
+                    className="btn btn-secondary dropdown-toggle"
+                    data-toggle="dropdown"
+                    data-bs-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false">
+                    Estado: {showPending ? "Pendientes " : "Aprobados "}
+                  </button>
+                  <div className="dropdown-menu">
+                    <button
+                      className="dropdown-item"
+                      type="button"
+                      onClick={() => {
+                        setCurrState(showPending ? "Accepted" : "Pending");
+                        setShowPending(!showPending);
+                      }}>
+                      {showPending ? "Aprobados" : "Pendientes"}
+                    </button>
+                  </div>
+                </div>
+                <button
+                  className="btn btn-primary"
+                  type="button"
+                  disabled={!canMakeSearch()}
+                  onClick={canMakeSearch() ? filterInquiries : undefined}>
+                  Buscar
+                </button>
+              </div>
             </div>
           </div>
-          <div className="col-md-4">
-            <div className="dropdown">
-              <button
-                className="btn btn-secondary dropdown-toggle project-active-dropdown"
-                type="button"
-                id="tagsDropdown"
-                data-bs-toggle="dropdown"
-                aria-expanded="false">
-                Activo
-              </button>
-              <ul className="dropdown-menu" aria-labelledby="tagsDropdown">
-                {/* {active.map((active, index) => (
-                  <li key={index}>
-                    <a className="dropdown-item" href="#">
-                      <div className="form-check">
-                        <input
-                          className="form-check-input"
-                          type="checkbox"
-                          value={active ? "true" : "false"}
-                          checked={searchActive.includes(String(active))}
-                          onChange={() =>
-                            setSearchActive((prevSelected) => {
-                              if (prevSelected.includes(String(active))) {
-                                return prevSelected.filter(
-                                  (a) => a !== String(active)
-                                );
-                              } else {
-                                return [...prevSelected, String(active)];
-                              }
-                            })
-                          }
-                        />
-                        <label className="form-check-label">
-                          {active ? "Sí" : "No"}
-                        </label>
-                      </div>
-                    </a>
-                  </li>
-                ))} */}
-              </ul>
-            </div>
-          </div>
-          <button type="submit" className="btn btn-primary search-btn">
-            Search
-          </button>
         </div>
       </form>
-      <div className="project-table">
-        <table className="table table-bordered">
+      <div className="inquiries-table">
+        <table className="table table-bordered table-responsive">
           <thead>
             <tr>
               <th>Solicitud</th>
@@ -142,15 +102,17 @@ const InquiriesList = () => {
             </tr>
           </thead>
           <tbody>
-            {filteredInquiries.map((inquiry) => (
+            {inquiries.map((inquiry) => (
               <tr key={inquiry._id}>
                 <td>
-                  <Link to={`/inquiries-registry/${inquiry._id}`}>{inquiry.projectName}</Link>
+                  <Link to={`/inquiries-registry/${inquiry._id}`}>
+                    {inquiry.projectName}
+                  </Link>
                 </td>
                 <td>{inquiry.projectRequester.name}</td>
                 <td>{inquiry.projectRequester.email}</td>
               </tr>
-            ))}            
+            ))}
           </tbody>
         </table>
       </div>
