e8c0609f92aa07330b08b6dc15ddfdb3dc4dd395
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2023-10-14 10:27:22 -0600

    Enable displaying the filtered requests

--- Front-End/src/components/organisms/InquiriesList/InquiriesList.tsx
+++ Front-End/src/components/organisms/InquiriesList/InquiriesList.tsx
@@ -1,19 +1,30 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { IInquiry, InquiryStatus, getFilteredInquiries } from "./inquiries";
+import { Link, useSearchParams } from "react-router-dom";
+import {
+  IInquiry,
+  InquiryStatus,
+  getFilteredInquiries,
+  getShowState,
+} from "./inquiries";
 import "./InquiriesList.scss";
 import Loader from "../../molecules/Loader/Loader";
 
 const InquiriesList = () => {
-  const [inquiries, setInquiries] = useState<Array<IInquiry>>([]);
-  const [lastSearchTerm, setLastSearchTerm] = useState("");
-  const [currSearchTerm, setCurrSearchTerm] = useState("");
-  const [lastState, setLastState] = useState("Pending");
-  const [currState, setCurrState] = useState("Pending");
-  const [showPending, setShowPending] = useState(true);
+  // URL search params
+  const [searchParams, setSearchParams] = useSearchParams();
+  const projectName = searchParams.get("projectName");
+  const status = searchParams.get("status");
+
+  // Component state
+  const [inquiries, setInquiries] = useState<IInquiry[]>([]);
+  const [lastSearchTerm, setLastSearchTerm] = useState(projectName ?? "");
+  const [currSearchTerm, setCurrSearchTerm] = useState(projectName ?? "");
+  const [lastState, setLastState] = useState(status ?? "Pending");
+  const [currState, setCurrState] = useState(status ?? "Pending");
+  const [showState, setShowState] = useState(getShowState(status));
   const [loading, setLoading] = useState(false);
 
-  // Function to handle form submission
+  /** Handle the form submission */
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     filterInquiries();
@@ -24,15 +35,24 @@ const InquiriesList = () => {
   }, []);
 
   /** Filters inquiries by project name and status */
-  const filterInquiries = async () => {
+  const filterInquiries = async (
+    projectName: string = currSearchTerm,
+    requestShowState: InquiryStatus = showState,
+    currentState: string = currState,
+    currentSearchTerm: string = currSearchTerm
+  ) => {
     setLoading(true);
     const inquiries: Array<IInquiry> = await getFilteredInquiries(
-      currSearchTerm,
-      showPending ? InquiryStatus.PENDING : InquiryStatus.ACCEPTED
+      projectName,
+      requestShowState
     );
-    setLastState(currState);
+    setLastState(currentState);
     setInquiries(inquiries);
-    setLastSearchTerm(currSearchTerm);
+    setLastSearchTerm(currentSearchTerm);
+    setSearchParams({
+      ...(currentSearchTerm !== "" && { projectName: currSearchTerm }),
+      status: requestShowState,
+    });
     setLoading(false);
   };
 
@@ -40,6 +60,20 @@ const InquiriesList = () => {
   const canMakeSearch = () =>
     (currSearchTerm !== "" && currSearchTerm !== lastSearchTerm) ||
     currState !== lastState;
+
+  /** Gets the requests status names in Spanish */
+  const getStatusName = (status: InquiryStatus) => {
+    switch (status) {
+      case InquiryStatus.PENDING:
+        return "Pendientes";
+      case InquiryStatus.ACCEPTED:
+        return "Aprobadas";
+      case InquiryStatus.REJECTED:
+        return "Rechazadas";
+      default:
+        return "Pendientes";
+    }
+  };
 
   return (
     <div>
@@ -66,25 +100,64 @@ const InquiriesList = () => {
                     data-bs-toggle="dropdown"
                     aria-haspopup="true"
                     aria-expanded="false">
-                    Estado: {showPending ? "Pendientes " : "Aprobados "}
+                    Estado: {getStatusName(showState)}{" "}
                   </button>
                   <div className="dropdown-menu">
                     <button
                       className="dropdown-item"
                       type="button"
                       onClick={() => {
-                        setCurrState(showPending ? "Accepted" : "Pending");
-                        setShowPending(!showPending);
+                        setCurrState("Pending");
+                        setShowState(InquiryStatus.PENDING);
                       }}>
-                      {showPending ? "Aprobados" : "Pendientes"}
+                      Pendientes
+                    </button>
+                    <button
+                      className="dropdown-item"
+                      type="button"
+                      onClick={() => {
+                        setCurrState("Accepted");
+                        setShowState(InquiryStatus.ACCEPTED);
+                      }}>
+                      Aprobados
+                    </button>
+                    <button
+                      className="dropdown-item"
+                      type="button"
+                      onClick={() => {
+                        setCurrState("Rejected");
+                        setShowState(InquiryStatus.REJECTED);
+                      }}>
+                      Rechazados
                     </button>
                   </div>
                 </div>
                 <button
+                  className="btn btn-danger"
+                  type="button"
+                  onClick={() => {
+                    // Clear the text input
+                    const input = document.querySelector(
+                      "#inquiry-search-form input"
+                    ) as HTMLInputElement;
+                    input.value = "";
+
+                    setLastSearchTerm("");
+                    setCurrSearchTerm("");
+                    setLastState("Pending");
+                    setCurrState("Pending");
+                    setShowState(InquiryStatus.PENDING);
+                    filterInquiries("", InquiryStatus.PENDING, "Pending", "");
+                  }}>
+                  Borrar filtros
+                </button>
+                <button
                   className="btn btn-primary"
                   type="button"
                   disabled={!canMakeSearch()}
-                  onClick={canMakeSearch() ? filterInquiries : undefined}>
+                  onClick={
+                    canMakeSearch() ? () => filterInquiries() : undefined
+                  }>
                   Buscar
                 </button>
               </div>
